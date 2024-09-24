2c985355077a3dff247a901baeab99ad76c5ede5
k-cardon <cardon.kristen.n@gmail.com>
2024-09-23 21:42:45 -0700

    limit authorization to certain emails

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,37 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { DefaultSession, NextAuthOptions } from "next-auth";
 
-export const authOptions = {
-    providers: [
-        GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID!,
-          clientSecret: process.env.GOOGLE_API_SECRET!,
-          authorization: {
-            params: {
-              prompt: "consent",
-              access_type: "offline",
-              response_type: "code",
-            },
-          },
-        }),
-      ],}
+// Parse the authorized emails from the environment variable
+const authorizedUsers = process.env.AUTHORIZED_EMAILS?.split(',') || [];
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_API_SECRET!,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      // Check if user is defined and has an email
+      if (user?.email && authorizedUsers.includes(user.email)) {
+        return true; // Allow sign-in
+      }
+      return false; // Deny sign-in
+    },
+  },
+};
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
+
+
